02b0939aff5c8f66c591debb6f96777cc768d8df
Yassine Asghar <86357052+yassineasghar@users.noreply.github.com>
2022-11-10 20:49:29 +0100

    Create Blatt1-4.c

---
+++ Blatt1-4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    // Variable :
+    float Fallzeit , Vmax_ms , Vmax_kmh , h , g = 9.81 ;
+        // Header :
+   
+
+        // Engabe der Höhe :
+        printf("\n> Bitte gib die Höhe in metern eine:\t");
+        scanf("%f",&h);
+        printf("\n");
+        
+        // Rechnungsformeln :
+            //Fallzeit :
+                Fallzeit = sqrt((2*h)/g) ;
+            //Endgeschwindigkeit Vmax in m.s⁻¹ :
+                Vmax_ms = sqrt(2*g*h) ;
+            //Endgeschwindigkeit Vmax in km.h⁻¹ :
+                Vmax_kmh = Vmax_ms*3.6 ;
+    
+        // Ausgabe :
+    printf("> Die Endegeschwindigkeit in m.s⁻¹ beträgt :\tVmax = %.3f m.s⁻¹ \n\n",Vmax_ms);
+    printf("> Die Endegeschwindigkeit in km.h⁻¹ beträgt :\tVmax = %.3f km.h⁻¹\n\n",Vmax_kmh);
+    printf("> Die Fallzeit  beträgt :\tFallzeit = %.3f s \n\n",Fallzeit);
+    
+
+
+
+
+    
+    
+}
