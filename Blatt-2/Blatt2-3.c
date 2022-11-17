19848bde324d42d9d32bcf0ae9e96daaea819985
Yassine Asghar <86357052+yassineasghar@users.noreply.github.com>
2022-11-17 17:27:03 +0100

    Update Blatt2-3.c

--- Blatt-2/Blatt2-3.c
+++ Blatt-2/Blatt2-3.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <complex.h>
 
 
 int main(void) {
-    
+
     // Vars :
-    float a ,b ,c , D , X ,  X1 , X2 ;
-    
-    
-    // Header :
-    printf("====================\n");
-    printf("= ax² + bx + c = 0 =\n");
-    printf("====================\n");
-    
-    // Einlesen :
+    float a ,b ,c , p ,q , D, X ,X1 , X2 ,X11 , X12 ,X21 ,X22;
+
+    // Einlese :
     printf("Bitte gib den  Koeffizient a ein:\t");
     scanf("%f", &a);
     printf("Bitte gib den  Koeffizient b ein:\t");
@@ -23,25 +16,32 @@ int main(void) {
     printf("Bitte gib den  Koeffizient c ein:\t");
     scanf("%f", &c);
 
-    
-    D = b*b - 4*a*c ;
-    
+    p = b/a ;
+    q = c/a ;
+
+    D = ((p*p)/4)-q ;
+
     if(D == 0){
-        X = (-b)/(2*a);
+        X = (-p)/2;
         printf("> Die quadratische Gleichung hat 1 reelle Lösung:\t X = %.2f \n", X);
     }
-    
+
     else if(D > 0){
-        X1 = ((-b)- sqrt(D))/(2*a);
-        X2 = ((-b)+ sqrt(D))/(2*a);
+        X1 = ((-p)/2)-sqrt(D);
+        X2 = ((-p)/2)+sqrt(D);
         printf("> Die quadratische Gleichung hat 2 reelle Lösungen :\t X1 = %.2f und X2 = %.2f \n",X1 ,X2);
     }
     else{
-        X1 = ((-b)+ I*sqrt(-D))/(2*a);
-        X2 = ((-b)- I*sqrt(-D))/(2*a);
-        printf("> Die quadratische Gleichung hat 2 komplexe Lösungen:\t X1 = %.2f und X2 = %.2f \n",X1 ,X2);
+        X11 = ((-p)/2) ;
+        X21 = ((-p)/2) ;
+        X12 = -sqrt(-D) ;
+        X22 = sqrt(-D) ;
+
+        printf("> Die quadratische Gleichung hat 2 komplexe Lösungen:\n ");
+        printf("X1 = %.2f %.2fi und X2 = %.2f+%.2fi \n",X11 , X12 ,X21 ,X22);
     }
 
-    
-    return 0;
+
+return 0 ;
 }
+
