c2b9319a5eca9cf9a5aad4811bf81aacb8542aa9
Yassine Asghar <86357052+yassineasghar@users.noreply.github.com>
2022-11-18 01:00:19 +0100

    Update Blatt2-2.c

--- Blatt-2/Blatt2-2.c
+++ Blatt-2/Blatt2-2.c
@@ -5,41 +5,59 @@
 
 int main(void){
     
-    float Weight ;
+    float Abnahme ;
+    float LP1 , LP2 , LP3 ; // LP --> Listenpreis
+    float RR1 , RR2 , RR3 ; // RR --> Rabatt auf Rechnungsbetrag
     float Angebot1 , Angebot2 , Angebot3 ;
     
+    
+    
     printf("Bitte gib die Abnahmemenge ein :\t");
-        scanf("%f", &Weight);
-    while (Weight <0) {
-        printf("Inkorrekte Eingabe , Bitte nochmal versuchen :\t");
-        scanf("%f",&Weight);
-    }
+        scanf("%f", &Abnahme);
+    
+    
     
     // Preis 1 :
-    if(5000 <= Weight){
-        Angebot1 = ((Weight/50)*25.5) - ((Weight/50)*25.5*0.03) ;
-        printf("Preis 1 = %.2f\n", Angebot1);
+    LP1 = (Abnahme/50)*25.5 ;
+    RR1 = LP1 - LP1*0.03 ;
+    
+    if(5000 <= Abnahme){
+        Angebot1 = RR1 - RR1*0.00 ;
+        printf("Preis 1 = %.2f €\n", Angebot1);
         
-    }else{
-        Angebot1 = (Weight/50)*25.5 ;
-        printf("Preis 1 = %.2f\n",Angebot1);
     }
+    
+    else{
+        Angebot1 = LP1 ;
+        printf("Preis 1 = %.2f €\n",Angebot1);
+    }
+    
     // Preis 2 :
-    if(15000 <= Weight){
-        Angebot2 = ((Weight/50)*26.8) - ((Weight/50)*26.8*0.05) - ((Weight/50)*26.8*0.03);
-        printf("Preis 2 = %.2f\n", Angebot2);
-    }else{
-        Angebot2 = ((Weight/50)*26.8) - ((Weight/50)*26.8*0.03) ;
-        printf("Preis 2 = %.2f\n",Angebot2);
+    LP2 = (Abnahme/50)*26.8 ;
+    RR2 = LP2 - LP2*0.05 ;
+    
+    if(15000 <= Abnahme){
+        Angebot2 = RR2 - RR2*0.03;
+        printf("Preis 2 = %.2f €\n", Angebot2);
     }
+    
+    else{
+        Angebot2 = LP2 - LP2*0.03 ;
+        printf("Preis 2 = %.2f €\n",Angebot2);
+    }
+    
     // Preis 3 :
-    if(3000 <= Weight){
-        Angebot3 = ((Weight/50)*26.45) - ((Weight/50)*26.45*0.035) - ((Weight/50)*26.45*0.02);
-        //- ((Weight/50)*26.45*0.02);
-        printf("Preis 3 = %.2f\n", Angebot3);
-    }else{
-        Angebot3 = ((Weight/50)*26.45) - ((Weight/50)*26.45*0.02) ;
-        printf("Preis 3 = %.2f\n",Angebot3);
+    LP3 = (Abnahme/50)*26.45 ;
+    RR3 = LP3 - LP3*0.035 ;
+    
+    if(3000 <= Abnahme){
+        Angebot3 = RR3 - RR3*0.02;
+        printf("Preis 3 = %.2f €\n", Angebot3);
+    }
+    
+    else{
+        Angebot3 = LP3 - LP3*0.02 ;
+        printf("Preis 3 = %.2f €\n",Angebot3);
     }
     
     
